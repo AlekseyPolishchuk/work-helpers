29364dcaedfe42ff380129eb823ef955d23307d9
Alex Polishchuk <alex.polishchuk.dev@gmail.com>
2024-05-17 22:02:23 +0300

    add copy btn

--- app/js/script.js
+++ app/js/script.js
@@ -5,27 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
         const inputData = document.querySelector('.converter__input');
         const outputData = document.querySelector('.converter__output');
         const btn = document.querySelector('.converter__button');
+        const copyBtn = document.querySelector('.converter__copy');
 
         function formatKeys(inputText) {
-            // Разделяем входной текст на строки
             const lines = inputText.split('\n');
 
-            // Используем регулярное выражение для фильтрации нужных строк и извлечения ключей
             const keys = lines
-                .map(line => line.trim()) // Удаляем пробелы в начале и конце строки
-                .filter(line => /origin\/[A-Z]+-\d+/.test(line)) // Оставляем только нужные строки
-                .map(line => line.split('/')[1]); // Извлекаем ключ
+                .map(line => line.trim())
+                .filter(line => /origin\/[A-Z]+-\d+/.test(line))
+                .map(line => line.split('/')[1]);
 
-            // Формируем итоговую строку
             const result = keys.map(key => `key = ${key}`).join(' OR ');
 
             return result;
         }
 
         btn.addEventListener('click', () => {
-            const inputText = inputData.value; // Получаем текст из inputData
-            const formattedText = formatKeys(inputText); // Форматируем текст
-            outputData.value = formattedText; // Присваиваем результат в outputData
+            const inputText = inputData.value;
+            const formattedText = formatKeys(inputText);
+            outputData.value = formattedText;
+            copyBtn.classList.remove('copied');
+            copyBtn.innerText = 'Copy to Clipboard';
+        });
+
+        copyBtn.addEventListener('click', () => {
+            navigator.clipboard
+                .writeText(outputData.value)
+                .then(() => {
+                    copyBtn.classList.add('copied');
+                    copyBtn.innerText = 'Copied';
+                })
+                .catch(err => {
+                    console.error('Failed to copy text: ', err);
+                });
         });
     })();
 });
