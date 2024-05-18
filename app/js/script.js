cb8120fcd5b954d626f12dd20b03687e08f07916
Alex Polishchuk <alex.polishchuk.dev@gmail.com>
2024-05-18 10:21:40 +0300

    some changes

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const outputData = document.querySelector('.converter__output');
         const btn = document.querySelector('.converter__button');
         const copyBtn = document.querySelector('.converter__copy');
+        const clearBtn = document.querySelector('.converter__clear');
+        const list = document.querySelector('.converter__list');
 
         function formatKeys(inputText) {
             const lines = inputText.split('\n');
@@ -20,24 +22,97 @@ document.addEventListener('DOMContentLoaded', () => {
             return result;
         }
 
-        btn.addEventListener('click', () => {
-            const inputText = inputData.value;
+        function generateCheckboxList(keys) {
+            list.innerHTML = '';
+            keys.forEach(key => {
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = key;
+                checkbox.value = key;
+
+                const label = document.createElement('label');
+                label.htmlFor = key;
+                label.textContent = key;
+
+                const div = document.createElement('div');
+                div.classList.add('converter__checkbox');
+                div.appendChild(checkbox);
+                div.appendChild(label);
+
+                list.appendChild(div);
+            });
+        }
+
+        function updateOutputData(inputText) {
             const formattedText = formatKeys(inputText);
             outputData.value = formattedText;
-            copyBtn.classList.remove('copied');
-            copyBtn.innerText = 'Copy to Clipboard';
-        });
 
-        copyBtn.addEventListener('click', () => {
             navigator.clipboard
                 .writeText(outputData.value)
                 .then(() => {
                     copyBtn.classList.add('copied');
-                    copyBtn.innerText = 'Copied';
+                    copyBtn.textContent = 'Copied';
                 })
                 .catch(err => {
                     console.error('Failed to copy text: ', err);
                 });
+        }
+
+        function updateOutputAndList() {
+            outputData.classList.remove('error');
+            const inputText = inputData.value;
+
+            if (inputText.trim() === '') {
+                outputData.value = '';
+                list.innerHTML = '';
+                copyBtn.classList.remove('copied');
+                copyBtn.textContent = 'Copy to Clipboard';
+                return;
+            }
+
+            const inputKeys = inputText
+                .split('\n')
+                .map(line => line.trim())
+                .filter(line => /origin\/[A-Z]+-\d+/.test(line))
+                .map(line => line.split('/')[1]);
+
+            if (inputKeys.length === 0) {
+                outputData.classList.add('error');
+                outputData.value = 'Input data does not match the required format...';
+                list.innerHTML = '';
+                copyBtn.classList.remove('copied');
+                copyBtn.textContent = 'Copy to Clipboard';
+                return;
+            }
+
+            updateOutputData(inputText);
+
+            const formattedText = formatKeys(inputText);
+            outputData.value = formattedText;
+
+            const keys = formattedText.split(' OR ').map(item => item.split(' = ')[1]);
+            generateCheckboxList(keys);
+        }
+
+        inputData.addEventListener('input', updateOutputAndList);
+        inputData.addEventListener('focus', () => {
+            navigator.clipboard
+                .readText()
+                .then(text => {
+                    inputData.value = text;
+                    updateOutputAndList(); // Вызовите функцию для обновления данных и списка чекбоксов
+                })
+                .catch(err => {
+                    console.error('Failed to read from clipboard: ', err);
+                });
+        });
+
+        clearBtn.addEventListener('click', () => {
+            inputData.value = '';
+            outputData.value = '';
+            list.innerHTML = '';
+            copyBtn.classList.remove('copied');
+            copyBtn.textContent = 'Copy to Clipboard';
         });
     })();
 });
